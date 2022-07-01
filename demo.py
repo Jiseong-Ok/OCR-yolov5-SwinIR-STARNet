22e72f99a45bb96629e2b4ee8e20733ec842e5da
Jiseong-Ok <106142675+Jiseong-Ok@users.noreply.github.com>
2022-07-01 12:32:30 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -257,7 +257,6 @@ if __name__ == '__main__':
     parser.add_argument('--image_path', required=True, help='path to image which contains text images')
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
     parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
-    parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
     """ Data processing """
     parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
     parser.add_argument('--imgH', type=int, default=64, help='the height of the input image')
